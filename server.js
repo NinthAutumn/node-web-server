6ccfdfa45f61edab7e956109f2e71f379744c208
Arif Iwamoto <arif.iwamoto@gmail.com>
2018-12-02 16:25:27 +0900

    good learning experience

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
 var app = express();
-
+const fs = require('fs');
 hbs.registerPartials(__dirname + "/views/partials")
 
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
 app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", (err) => {
+    if (err) {
+      console.log("unable to append to server.log.")
+    }
+  })
+  next();
+});
+
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
